13064fcec8ebc4f90d8a086e08b4935e00ec097c
Muhammad Izzuddin Al Fikri <azura.azureus.ikhie@gmail.com>
2020-12-07 09:42:17 +0700

    Fix allowedEager expression

--- src/services/users/users.service.js
+++ src/services/users/users.service.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     Model: createModel(app),
     paginate: app.get('paginate'),
     whitelist: ['$eager', '$joinRelation'],
-    allowedEager: ['student', 'teacher']
+    allowedEager: '[student, teacher]'
   };
 
   // Initialize our service with any options it requires
